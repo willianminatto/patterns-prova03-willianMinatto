09970b4196a161f707bf1efa6d2c54cb3e654601
Willian Minatto <willian.279222@alunosatc.edu.br>
2025-10-30 19:22:41 -0300

    prova

--- desafio02/index01.js
+++ desafio02/index01.js
@@ -1,1 +1,24 @@
-console.log("Hello, World! 001");
+class TV {
+    turnOn() {
+      console.log("TV ligada.");
+    }
+    turnOff() {
+      console.log("TV desligada.");
+    }
+  }
+  
+  class RemoteControl {
+    constructor() {
+      this.tv = new TV();
+    }
+  
+    pressPowerButton(on) {
+      if (on) this.tv.turnOn();
+      else this.tv.turnOff();
+    }
+  }
+  
+  const remote = new RemoteControl();
+  remote.pressPowerButton(true);
+  remote.pressPowerButton(false);
+  
--- desafio02/index02.js
+++ desafio02/index02.js
@@ -1,1 +1,34 @@
-console.log("Hello, World! 002");
+class Task {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    showDetails() {
+      console.log(`Tarefa: ${this.name}`);
+    }
+  }
+  
+  class Project {
+    constructor(name) {
+      this.name = name;
+      this.tasks = [];
+    }
+  
+    add(task) {
+      this.tasks.push(task);
+    }
+  
+    showDetails() {
+      console.log(`Projeto: ${this.name}`);
+      this.tasks.forEach((task) => console.log(`  - ${task.name}`));
+    }
+  }
+  
+  const t1 = new Task("Escrever documentação");
+  const t2 = new Task("Fazer testes");
+  const p = new Project("Lançamento v1.0");
+  p.add(t1);
+  p.add(t2);
+  
+  p.showDetails();
+  
